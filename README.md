6fa268b83d662f306714a434724b6f30eaf4d764
윤준서 <58902513+liljuice@users.noreply.github.com>
2024-02-07 22:18:17 +0900

    Feat: First Commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Simple ATM Controller
+
+### Introduction
+- Account Class   
+: 계좌 번호와 잔고
+
+- Card Class   
+: 카드 번호와 PIN 번호, 연결된 계좌 번호   
+
+- Bank Class   
+: 계좌와 카드를 등록하고(테스트를 위해) 관리하는 기능   
+카드의 PIN 번호를 확인하여 인증하고, 카드에 연결된 계좌를 보여주는 기능   
+
+- Controller Class   
+: 고객이 카드를 삽입하고, PIN 번호를 입력하여 인증하고, 계좌를 선택하고, 예금 및 출금 기능을 수행   
+카드의 PIN 번호를 알아야 계좌 정보를 알 수 있으며, 그 계좌의 순번을 입력해서 계좌에 접근하므로 계좌 번호만 안다고 예금,출금을 수행할 수 없음   
+
+- Main Class   
+: 은행과 컨트롤러를 생성, 테스트용 계좌와 카드를 등록   
+사용자가 정보를 입력하는 객체로, Insert Card => PIN number => Select Account => See Balance/Deposit/Withdraw 의 Flow를 CLI로 사용자가 파악할 수 있도록 함.
+
+
+### Instructions
+---
+Requirements : Python 3
+
+1) 리포지토리 복사하기
+```
+git clone https://github.com/liljuice/simpleATM.git
+```
+2) 실행하기
+```
+python3 simpleATM.py
+```
+### Test Cases
+---
+다음 테스트 케이스가 입력되어 있습니다.
+
+|Account Number|Balance|
+|------|---|
+|12345678|$50000|
+|20240207|$0|
+|11111111|$300000|
+
+|Card Number|PIN Number|Linked Accounts List|
+|------|---|-----|
+|1234|5678|12345678|
+|2024|0207|20240207, 11111111|
+
+### ETC
+---
+ATM 컨트롤러의 Cash bin의 잔고와 같은 부분은 고려하지 않았습니다.   
+
+
---
+++ simpleATM.py
@@ -0,0 +1,200 @@
+class Account:
+    def __init__(self, accountNumber, balance):
+        self.accountNumber = accountNumber
+        self.balance = balance
+
+class Card:
+    def __init__(self, cardNumber, PIN):
+        self.cardNumber = cardNumber
+        self.pin = PIN
+        self.accounts = []
+    
+    # 카드에 계좌 연결 (테스트용)
+    def addAccount(self, accountNumber):
+        self.accounts.append(accountNumber)
+    
+    # 카드에 연결된 계좌
+    def getAccount(self):
+        return self.accounts
+
+class Bank:
+    def __init__(self):
+        self.accounts = {}
+        self.cards = {}
+    
+    # (테스트용) 계좌 정보 등록
+    def registerAccount(self, accountNumber, balance):
+        account = Account(accountNumber, balance)
+        self.accounts[accountNumber] = account
+        
+    # (테스트용) 카드 정보 등록
+    def registerCard(self, cardNumber, PIN):
+        card = Card(cardNumber, PIN)
+        self.cards[cardNumber] = card
+        
+    # (테스트용) 카드에 계좌 연결
+    def addAccToCard(self, cardNumber, accountNumber):
+        card = self.cards.get(cardNumber)
+        if card:
+            card.addAccount(accountNumber)
+        else:
+            print("no card")
+        
+    # 카드 넘버에 대한 PIN 번호가 일치하는지 확인
+    def checkPIN(self, cardNumber, inputPIN):
+        card = self.cards.get(cardNumber)
+        if card:
+            return card.pin == inputPIN
+        else:
+            return False
+
+    # 카드에 연결된 계좌 확인
+    def getAccount(self, cardNumber):
+        card = self.cards.get(cardNumber)
+        if card:
+            return card.getAccount()
+        else:
+            return None
+        
+    # 잔고
+    def balance(self, accountNumber):
+        account = self.accounts.get(accountNumber)
+        if account:
+            return account.balance
+        else:
+            print("해당 계좌가 존재하지 않습니다.")
+            return None
+    
+    # 예금(잔고 추가)
+    def deposit(self, accountNumber, amount):
+        account = self.accounts.get(accountNumber)
+        if account:
+            account.balance += amount
+            print("예금이 성공적으로 처리되었습니다.")
+        else:
+            print("해당 계좌가 존재하지 않습니다.")
+    
+    # 출금(잔고 감소)
+    def withdraw(self, accountNumber, amount):
+        account = self.accounts.get(accountNumber)
+        if account:
+            if account.balance >= amount:
+                account.balance -= amount
+                print("출금이 성공적으로 처리되었습니다.")
+                return True
+            else:
+                print("잔고가 부족합니다.")
+                return False
+        else:
+            print("해당 계좌가 존재하지 않습니다.")
+            return False
+    
+class Controller:
+    def __init__(self, bank):
+        self.bank = bank
+        self.accountNumber = None
+        self.cardNumber = None
+        self.certified = False
+    
+    # 고객의 카드 번호를 저장
+    def insertCard(self, cardNumber):
+        self.cardNumber = cardNumber
+        
+    def selectAccount(self, accountNumber):
+        self.accountNumber = accountNumber
+        
+    # 고객의 PIN 번호를 입력받아 올바른지 확인
+    def enterPIN(self, PIN):
+        if self.bank.checkPIN(self.cardNumber, PIN):
+            self.certified = True
+            return True
+        else:
+            self.certified = False
+            return False
+
+    
+    def checkBalance(self):
+        balance = self.bank.balance(self.accountNumber)
+        return balance
+    
+    def deposit(self, amount):
+        self.bank.deposit(self.accountNumber, amount)
+    
+    def withdraw(self, amount):
+        isWork = self.bank.withdraw(self.accountNumber, amount)
+        return isWork
+        
+    
+class Main:
+    def run(self):
+        bank = Bank()
+        controller = Controller(bank)
+        
+        # 테스트를 위함
+        bank.registerAccount('12345678', 50000)
+        bank.registerAccount('20240207', 0)
+        bank.registerAccount('11111111', 300000)
+        bank.registerCard('1234', '5678')
+        bank.registerCard('2024', '0207')
+        bank.addAccToCard('1234', '12345678')
+        bank.addAccToCard('2024', '20240207')
+        bank.addAccToCard('2024', '11111111')
+        print(bank.accounts)
+        print(bank.cards)
+        while True:
+            print("======= Simple ATM Controller =======")
+            print("카드를 넣어주세요. (카드 번호를 입력하세요.)")
+            print("종료하시려면, 0을 입력해주세요.")
+            cmdFirst = input()
+            if cmdFirst == '0':
+                break
+            controller.insertCard(cmdFirst)
+            print("PIN 번호를 입력하세요.")
+            
+            if controller.enterPIN(input().rstrip()):
+                print("확인되었습니다.")
+                
+                print("다음은 연결되어 있는 계좌입니다.")
+                acc = bank.getAccount(cmdFirst)
+
+                for i in range(len(acc)):
+                    print("계좌", i+1, "번 : ", acc[i])
+                    
+                n = int(input("이용하실 계좌의 순번을 입력하세요: "))
+                if n < 1 or n > len(acc) + 1:
+                    print("올바르지 않은 입력입니다.")
+                    break
+                else:
+                    controller.selectAccount(acc[n-1])
+                    
+                while True:
+                    print("원하시는 기능의 번호를 입력하세요.")
+                    print("1. 예금")
+                    print("2. 출금")
+                    print("3. 잔고 확인")
+                    print("0. 종료")
+                    command = input("입력: ")
+                    
+                    if command == '1':
+                        amt = int(input("입금할 금액: "))
+                        controller.deposit(amt)
+                        print("입금되었습니다.\n")
+                    elif command == '2':
+                        amt = int(input("출금할 금액: "))
+                        isWork = controller.withdraw(amt)
+                        if isWork:
+                            print("출금되었습니다.\n")
+                        else:
+                            print("출금에 실패했습니다.\n")
+                    elif command == '3':
+                        b = controller.checkBalance()
+                        print("고객님의 계좌 잔고는", b, "달러입니다.\n")
+                    elif command == '0':
+                        break
+            else:
+                print("카드와 PIN 번호가 일치하지 않습니다. 다시 시도해주세요.\n")
+        
+
+if __name__ == "__main__":
+    main = Main()
+    main.run()
